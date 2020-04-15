0f62eb4fc48c4c4876669c91bcb218108ddc8094
zcchenchen <782699991@qq.com>
2020-04-15 17:38:06 +0800

    idea提交

--- Chapter5/src/main/java/com/course/testng/suite/TimeOutTest.java
+++ Chapter5/src/main/java/com/course/testng/suite/TimeOutTest.java
@@ -7,7 +7,7 @@ public class TimeOutTest {
     @Test(timeOut = 3000)//单位为毫秒值
     public void testSuccess() throws InterruptedException{
         Thread.sleep(2000);
-        
+
     }
 
     @Test(timeOut = 2000)//单位为毫秒值
